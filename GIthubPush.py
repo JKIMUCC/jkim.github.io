30ea633feafb407a3e1eba83f66b8017e9ec6ea0
Jace Kim <jace.kim@0102.local>
2019-01-09 12:07:51 -0500

    new

---
+++ GIthubPush.py
@@ -0,0 +1,20 @@
+#Created by Justin Lo Sept 14 2018
+#Borrowed by JACE
+
+import os
+
+os.system("cd desktop")
+os.system("cd CS")
+os.system("cd Programming")
+os.system("cd GitRepoYear9")
+os.system("cd Year9DesignCS4-PythonJL")
+
+text=input('What is your comment for commit: ')
+
+os.system("git status")
+os.system("git add .")
+
+sentence = str('git commit -m "{0}"'.format(text))
+os.system(sentence)
+
+os.system("git push")
